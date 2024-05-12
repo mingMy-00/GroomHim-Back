23f2106b7292b75008de6a9275deacca5c88086b
mingMy-00 <wbsebqb1118@naver.com>
2024-05-12 15:03:39 +0900

    버전이 없는 파일

---
+++ GroomHim-Back/src/main/java/com/care/GroomHimBack/GroomHimBackApplication.java
@@ -0,0 +1,13 @@
+package com.care.GroomHimBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomHimBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomHimBackApplication.class, args);
+	}
+
+}
---
+++ GroomHim-Back/src/test/java/com/care/GroomHimBack/GroomHimBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.care.GroomHimBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomHimBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
