473c2b398b4a5e3b0d31f8dc434cc605992d6125
mingMy-00 <wbsebqb1118@naver.com>
2024-05-14 21:00:34 +0900

    userRepository 변경

--- GroomHim-Back/src/main/java/com/care/GroomHimBack/repository/UserRepository.java
+++ GroomHim-Back/src/main/java/com/care/GroomHimBack/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.care.GroomHimBack.repository;
 import com.care.GroomHimBack.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface USerRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
 
 }
